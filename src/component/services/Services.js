048e79fd2e2b11f3369e0169108c158dcd6b37e3
qais-alsgher <qaisalsgher@gmail.com>
2023-01-16 10:20:30 +0300

    bulit featuers and testmonals and team member part and design for it

---
+++ src/component/services/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './services.css';
+function Services() {
+  return (
+    <div className='services'>
+        <div className='container'>
+            <div className='box'>
+                </div>
+                </div>
+    </div>
+  )
+}
+
+export default Services;
